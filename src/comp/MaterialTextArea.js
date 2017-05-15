bc701e79fffff027dc57321c496f136adcbd22bf
Gabriel Aumala <gabrielfima@live.com>
2017-05-14 19:54:07 -0500

    Add MaterialTextArea component

---
+++ src/comp/MaterialTextArea.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+import './MaterialTextArea.css'
+
+export default class MaterialTextArea extends Component {
+  render() {
+    const {
+      inputRef,
+    } = this.props
+
+    return (
+      <div className="material-text">
+      	<textarea ref={inputRef} rows="5" className="material-text" type="text" required/>
+      	<hr className="material-text" />
+      	<label className="material-text">Comment</label>
+      </div>
+    )
+  }
+}
