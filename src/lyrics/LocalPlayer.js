7fe210b9f81c23966bc91e30cbc79bd627db3a28
Gabriel Aumala <gabrielfima@live.com>
2017-05-17 03:50:55 -0500

    Don't load videos until play button is pressed

--- src/lyrics/LocalPlayer.js
+++ src/lyrics/LocalPlayer.js
@@ -6,7 +6,7 @@ export default class LocalPlayer extends Component {
   render() {
     const url = `/video/${this.props.url}.mp4`
     return (
-      <video width="560" controls className='local-video'>
+      <video width="560" controls className='local-video' preload='none'>
         <source src={url} type="video/mp4" />
         Sorry, your browser does not support embedded videos,
         but do not worry, you can <a href={url}>download it</a>
